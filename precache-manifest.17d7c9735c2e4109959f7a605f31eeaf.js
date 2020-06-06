a8b2bb41f469e3ba192fc64157778b11c365b2e0
Arek <Arek@DESKTOP-U4VRK31>
2020-06-06 12:52:52 +0200

    Updates

---
+++ precache-manifest.17d7c9735c2e4109959f7a605f31eeaf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3c7d501b83b70afc01487e4b0ec4dc",
+    "url": "/PiW-lab5/index.html"
+  },
+  {
+    "revision": "bcac461225a2bf306fce",
+    "url": "/PiW-lab5/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fd2950a5c6645157f018",
+    "url": "/PiW-lab5/static/css/main.d6e23e5c.chunk.css"
+  },
+  {
+    "revision": "bcac461225a2bf306fce",
+    "url": "/PiW-lab5/static/js/2.c32ad0f9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PiW-lab5/static/js/2.c32ad0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2950a5c6645157f018",
+    "url": "/PiW-lab5/static/js/main.19b8ac20.chunk.js"
+  },
+  {
+    "revision": "02e74886aa70ebf55752",
+    "url": "/PiW-lab5/static/js/runtime-main.9a8f7865.js"
+  },
+  {
+    "revision": "e703654b17480c7013c290a5bd851739",
+    "url": "/PiW-lab5/static/media/logo.e703654b.svg"
+  },
+  {
+    "revision": "0e1a1222ea532d074f39850fbeeb8bf9",
+    "url": "/PiW-lab5/static/media/wolf.0e1a1222.png"
+  }
+]);
